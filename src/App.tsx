b81cd7d73e8b7c9d7aed1acb3b2726d406f29537
Albert Garuda <algarud@gmail.com>
2022-07-17 22:56:51 +0300

    sample chat func

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,39 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { StreamChat } from 'stream-chat';
+import { Chat, Channel, ChannelHeader, MessageInput, MessageList, Thread, Window } from 'stream-chat-react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import 'stream-chat-react/dist/css/index.css';
+
+const chatClient = StreamChat.getInstance('dz5f4d5kzrue');
+const userToken = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoiZGF3bi1zbm93LTgiLCJleHAiOjE2NTc5MDYzOTh9.KxlQEpFwyLvqWkq1Zy0iJBZGdomQCH3m3MKU7OlF1Yg';
+
+chatClient.connectUser(
+  {
+    id: 'dawn-snow-8',
+    name: 'dawn',
+    image: 'https://getstream.io/random_png/?id=dawn-snow-8&name=dawn',
+  },
+  userToken,
+);
+
+const channel = chatClient.channel('messaging', 'custom_channel_id', {
+  // add as many custom fields as you'd like
+  image: 'https://www.drupal.org/files/project-images/react.png',
+  name: 'Talk about React',
+  members: ['dawn-snow-8'],
+});
+
+const App = () => (
+  <Chat client={chatClient} theme='messaging light'>
+    <Channel channel={channel}>
+      <Window>
+        <ChannelHeader />
+        <MessageList />
+        <MessageInput />
+      </Window>
+      <Thread />
+    </Channel>
+  </Chat>
+);
 
 export default App;
