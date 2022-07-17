e41a3ff3eb3146976835957c6ac8911ca30109d5
Albert Garuda <algarud@gmail.com>
2022-07-17 23:21:07 +0300

    adjustments

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,53 @@
-import React from 'react';
-import { StreamChat } from 'stream-chat';
-import { Chat, Channel, ChannelHeader, MessageInput, MessageList, Thread, Window } from 'stream-chat-react';
+import React, { useEffect, useState } from 'react';
+import { DefaultGenerics, StreamChat } from 'stream-chat';
+import { Chat, Channel, ChannelHeader, ChannelList, LoadingIndicator, MessageInput, MessageList, Thread, Window } from 'stream-chat-react';
 
 import 'stream-chat-react/dist/css/index.css';
 
-const chatClient = StreamChat.getInstance('dz5f4d5kzrue');
 const userToken = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoiZGF3bi1zbm93LTgiLCJleHAiOjE2NTc5MDYzOTh9.KxlQEpFwyLvqWkq1Zy0iJBZGdomQCH3m3MKU7OlF1Yg';
 
-chatClient.connectUser(
-  {
-    id: 'dawn-snow-8',
-    name: 'dawn',
-    image: 'https://getstream.io/random_png/?id=dawn-snow-8&name=dawn',
-  },
-  userToken,
-);
+const filters = { type: 'messaging', members: { $in: ['dawn-snow-8'] } };
+const sort = { last_message_at: -1 };
 
-const channel = chatClient.channel('messaging', 'custom_channel_id', {
-  // add as many custom fields as you'd like
-  image: 'https://www.drupal.org/files/project-images/react.png',
-  name: 'Talk about React',
-  members: ['dawn-snow-8'],
-});
+const App = () => {
+  const [chatClient, setChatClient] = useState(null);
 
-const App = () => (
-  <Chat client={chatClient} theme='messaging light'>
-    <Channel channel={channel}>
-      <Window>
-        <ChannelHeader />
-        <MessageList />
-        <MessageInput />
-      </Window>
-      <Thread />
-    </Channel>
-  </Chat>
-);
+  useEffect(() => {
+    const initChat = async () => {
+      const client = StreamChat.getInstance('dz5f4d5kzrue');
+
+      await client.connectUser(
+        {
+          id: 'dawn-snow-8',
+          name: 'dawn',
+          image: 'https://getstream.io/random_png/?id=dawn-snow-8&name=dawn',
+        },
+        userToken,
+      );
+
+      setChatClient(client);
+    };
+
+    initChat();
+  }, []);
+
+  if (!chatClient) {
+    return <LoadingIndicator />;
+  }
+
+  return (
+    <Chat client={chatClient} theme='messaging light'>
+      <ChannelList filters={filters} sort={sort} />
+      <Channel>
+        <Window>
+          <ChannelHeader />
+          <MessageList />
+          <MessageInput />
+        </Window>
+        <Thread />
+      </Channel>
+    </Chat>
+  );
+};
 
 export default App;
