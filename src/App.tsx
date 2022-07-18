f6588c7bece16c81db6d609699371b41e26afab6
Albert Garuda <algarud@gmail.com>
2022-07-18 20:11:43 +0300

    simple layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import React, { useEffect, useState } from 'react';
 import { ChannelSort, DefaultGenerics, StreamChat } from 'stream-chat';
-import { Chat,      // no ui, general data about chat app
-          Channel,  // no ui, data about active channel
-          Window,   // container utility, responsiveness
-          ChannelHeader, // basic info about active channel
-          MessageList,  // list of messages in currently active channel
-          MessageInput,
-          Thread,       // manages threads
-          ChannelList, 
-          LoadingIndicator } from 'stream-chat-react';
+import {
+  Chat,      // no ui, general data about chat app
+  Channel,  // no ui, data about active channel
+  Window,   // container utility, responsiveness
+  ChannelHeader, // basic info about active channel
+  MessageList,  // list of messages in currently active channel
+  MessageInput,
+  Thread,       // manages threads
+  ChannelList,
+  LoadingIndicator
+} from 'stream-chat-react';
 import 'stream-chat-react/dist/css/index.css';
 
 const userToken = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoiZGF3bi1zbm93LTgiLCJleHAiOjE2NTc5MDYzOTh9.KxlQEpFwyLvqWkq1Zy0iJBZGdomQCH3m3MKU7OlF1Yg';
@@ -20,8 +22,7 @@ const user = {
   image: 'https://getstream.imgix.net/images/random_svg/PN.png'
 }
 
-
-const App = () => {
+export default function App() {
   const [client, setClient] = useState(null as (StreamChat<DefaultGenerics> | null));
   const [channel, setChannel] = useState(null);
 
@@ -43,10 +44,19 @@ const App = () => {
     init();
   }, [])
 
+  console.log('api key:', apiKey);
+  if (!channel || !client) return <LoadingIndicator />
 
   return (
-    <></>
+    <Chat client={client} theme="messaging dark">
+      <Channel channel={channel}>
+        <Window>
+          <ChannelHeader />
+          <MessageList />
+          <MessageInput />
+        </Window>
+        <Thread />
+      </Channel>
+    </Chat>
   );
 };
-
-export default App;
